e26a5513927514d59a9bf355c02149c0f88a89fc
chrisjp06 <chrisjp2506@gmail.com>
2026-07-01 02:33:27 +0530

    model and mflow

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
 
 if __name__ == "__main__":
     try:
@@ -33,6 +35,13 @@ if __name__ == "__main__":
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
+
+        logging.info("Model Training started")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
 
 
 
